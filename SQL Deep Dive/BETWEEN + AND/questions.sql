f3abc8d622af1387a5cc3b219b0c064cde924103
i4click <89821919+i4click@users.noreply.github.com>
2024-09-02 17:17:23 +0800

    between.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -2,10 +2,12 @@
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+SELECT * FROM customers
+WHERE income < 50000 AND age BETWEEN 30 AND 50;
 */
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
-* Write your query here
+SELECT AVG(income) AS average_income FROM customers
+WHERE age BETWEEN 20 AND 50;
 */
